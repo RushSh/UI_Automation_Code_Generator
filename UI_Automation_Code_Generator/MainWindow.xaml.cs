5f0f46bdc707e042bb6d2b62bdac26837e2a220b
Rushabh Shah <rushshah14@gmail.com>
2019-04-08 22:49:34 -0400

    Added TreeAndButton control

--- UI_Automation_Code_Generator/MainWindow.xaml.cs
+++ UI_Automation_Code_Generator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Interop.UIAutomationCore;
 
 namespace UI_Automation_Code_Generator
 {
@@ -23,6 +24,30 @@ namespace UI_Automation_Code_Generator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Refresh_Tree_Click(object sender, RoutedEventArgs e)
+        {
+            this.AutomationTree_TargetUpdated(sender, null);
+        }
+
+        private void AutomationTree_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            //IUIAutomationCondition uIAutomationCondition = new IUIAutomationCondition();
+            CUIAutomation8 cUIAutomation8 = new CUIAutomation8();
+            IUIAutomationElementArray rootChildrens = cUIAutomation8.GetRootElement().FindAll(TreeScope.TreeScope_Children, cUIAutomation8.CreateTrueCondition());
+            TreeViewItem tree = new TreeViewItem();
+            tree.Header = cUIAutomation8.GetRootElement().GetCurrentPropertyValue(30005);
+          //  IUIAutomationTreeWalker uIAutomationTreeWalker;
+           // uIAutomationTreeWalker.
+            for(int i = 0; i <rootChildrens.Length; i++)
+            {
+                IUIAutomationElement children = rootChildrens.GetElement(i);
+                tree.Items.Add(String.Format("{0}  {1}", children.GetCurrentPropertyValue(30005), children.CurrentLocalizedControlType));
+            }
+            automationTree.Items.Add(tree);
+            //MessageBox.Show(rootElement.GetCurrentPropertyValue(30012));
+
         }
     }
 }
---
+++ UI_Automation_Code_Generator/PropertyIdentifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Automation_Code_Generator
+{
+    interface PropertyIdentifier
+    {
+    }
+
+    enum PropertyList
+    {
+            AcceleratorKeyPropertyId = 30006,
+            AccessKeyPropertyId = 30007,
+            AnnotationObjectsPropertyId = 30156,
+            AnnotationTypesPropertyId = 30155,
+            AriaPropertiesPropertyId = 30102,
+            AriaRolePropertyId = 30101,
+            AutomationIdPropertyId = 30011,
+            BoundingRectanglePropertyId = 30001,
+            CenterPointPropertyId = 30165,
+            ClassNamePropertyId = 30012,
+            ClickablePointPropertyId = 30014,
+            ControllerForPropertyId = 30104,
+             
+
+    }
+}
